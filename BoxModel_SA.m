7647383aedaa01b06b6a4d54270da4d8532b58cf
Fateme Yousefi <fy23@NS-O203-FY23.win.duke.edu>
2017-02-06 16:27:47 -0500

    correction on marsh conersion to tf - not compete

--- BoxModel_SA.m
+++ BoxModel_SA.m
@@ -14,48 +14,52 @@ format longG
 if nargin == 0 || strcmp(parin,'all') || unique(parin == 0)
     par_temp = 1:14;
     nj = length(par_temp);
-else
+elseif ischar(parin) || length(parin) == 1
     par = parin;
     nj = 1;
+else
+    par_temp = parin;
+    nj = length(parin);
 end
 
 for j = 1 : nj
     
     tic
     
-    if nj>1
+    if nj > 1
         par = par_temp(j);
     end
     
     switch par
         case {'C_o' , 1}
-            C_o_ = [10*10^-3 20*10^-3 100*10^-3]; leg = {'10 g/m^3','20 g/m^3','100 g/m^3'}; tit = 'Ocean Concentration';
+            C_o_ = [10*10^-3, 20*10^-3, 100*10^-3]; leg = {'10 g/m^3','20 g/m^3','100 g/m^3'}; tit = 'Ocean Concentration';
         case {'C_f' , 2}
-            C_f_ = [10*10^-3 20*10^-3 100*10^-3]; leg = {'0 g/m^3','15 g/m^3','100 g/m^3'}; tit = 'River Concentration';
+            C_f_ = [10*10^-3, 20*10^-3, 100*10^-3]; leg = {'0 g/m^3','15 g/m^3','100 g/m^3'}; tit = 'River Concentration';
         case {'Q_f' , 3}
             Q_f_ = [0 20 1000]; leg = {'0 m^3/s','20 m^3/s','1000 m^3/s'}; tit = 'River Discharge';
         case {'tau_c' , 4}
             tau_c_ = [.1 .3 .4 ]; leg = {'0.1 PA','0.3 PA','0.4 PA'}; tit = 'Critical Shear Stress';
         case {'E_0' , 5}
-            E_0_ = [10^-5 10^-4 10^-3]; leg = {'10^{-5} kg/m^2/s','10^{-4} kg/m^2/s','10^{-3} kg/m^2/s'}; tit = 'Bed Erosion Coefficient';
+            E_0_ = [10^-5, 10^-4, 10^-3]; leg = {'10^{-5} kg/m^2/s','10^{-4} kg/m^2/s','10^{-3} kg/m^2/s'}; tit = 'Bed Erosion Coefficient';
         case {'k_e' , 6}
-            k_e_ = [0.1/365/24/60/60 0.16/365/24/60/60 0.2/365/24/60/60]; leg = {'0.1 m^2/yr/W','0.16 m^2/yr/W','0.2 m^2/yr/W'}; tit = 'Margin Erodibility Coefficient';
+            k_e_ = [0.1/365/24/60/60, 0.16/365/24/60/60, 0.2/365/24/60/60]; leg = {'0.1 m^2/yr/W','0.16 m^2/yr/W','0.2 m^2/yr/W'}; tit = 'Margin Erodibility Coefficient';
         case {'v_w' , 7}
             v_w_ = [0 6 10]; leg = {'0 m/s','6 m/s','10 m/s'}; tit = 'Wind Velocity';
         case {'k_a' , 8}
             k_a_ = [1 2 4]; leg = {'1','2','4'}; tit ='Margin Accretion Coefficient';
         case {'k_B' , 9}
-            k_B_ = [1*10^-3/365/24/60/60 2*10^-3/365/24/60/60 4*10^-3/365/24/60/60]; leg = {'1 m3/yr/g','2 m3/yr/g','4 m3/yr/g'}; tit = 'Biomass Coefficient';
+            k_B_ = [1*10^-3/365/24/60/60, 2*10^-3/365/24/60/60, 4*10^-3/365/24/60/60]; leg = {'1 m3/yr/g','2 m3/yr/g','4 m3/yr/g'}; tit = 'Biomass Coefficient';
         case {'R' , 10}
-            R_ = [0 2*10^-3/365/24/60/60 10*10^-3/365/24/60/60]; leg = {'0 mm/yr','2 mm/yr','10 mm/yr'}; tit = 'Rate of Sea Level Rise';
+            R_ = [0, 2*10^-3/365/24/60/60, 10*10^-3/365/24/60/60]; leg = {'0 mm/yr','2 mm/yr','10 mm/yr'}; tit = 'Rate of Sea Level Rise';
         case {'b_fm' , 11}
-            b_fm_ = [10^3 5*10^3 10*10^3];  leg = {'0.5 km','2.5 km','5 km'}; tit = 'Basin Width';
+            b_fm_ = [1*10^3, 5*10^3, 10*10^3];  leg = {'0.5 km','2.5 km','5 km'}; tit = 'Basin Width';
         case {'L_E' , 12}
-            L_E_ = [.15*10^3 15*10^3 150*10^3]; leg = {'0.15 km','15 km','150 km'}; tit = 'Estuary Length';
+            L_E_ = [0.15*10^3, 15*10^3, 150*10^3]; leg = {'0.15 km','15 km','150 km'}; tit = 'Estuary Length';
         case {'b_f_0' , 13}
-            b_f_0 = [1*10^3  5*10^3  9*10^3]/4; leg = {'0.25 km (0.1 L)','1.25 km (0.5 L)','2.25 km (0.9 L)'}; tit = 'Initial Tidal Flat Width';
+            b_f_0 = [1*10^3,  5*10^3,  9*10^3]/4; leg = {'0.25 km (0.1 L)','1.25 km (0.5 L)','2.25 km (0.9 L)'}; tit = 'Initial Tidal Flat Width';
+%             b_f_0 = [.75*10^3,  .80*10^3,  .85*10^3]; leg = {'0.25 km (0.1 L)','1.25 km (0.5 L)','2.25 km (0.9 L)'}; tit = 'Initial Tidal Flat Width';
         case {'H' , 14}
-            H_ = [1 1.4 2.4]/2; leg = {'1 m','1.4 m','2.4 m'}; tit = 'Tidal Range';
+            H_ = [1, 1.4, 1.8]/2; leg = {'1 m','1.4 m','1.8 m'}; tit = 'Tidal Range';
     end
     
     %-------------- Start the loop for each run
@@ -104,14 +108,8 @@ for j = 1 : nj
         gamma = 9800;   % water specific weight (N/m3)
         g = 9.81;       % gravitational acceleration (m/s2)
         
-        %-------------- Initial conditions, y0=[ b_f, d_f, d_m,u(=C_r*(b_f*d_f+b_m*d_m))]
-        y0(1) = b_fm/4;   % tidal flat width (m)
-        y0(2) = 1.0;         % tidal flat depth (m)
-        y0(3) = 0.4;         % marsh depth (m)
-        y0(4) = 0*10^-3*(y0(1)*(y0(2)+y0(3)));
-        
         %-------------- Parameter assignment
-        
+        flag_bf0 = 0;
         switch par
             case {'C_o' , 1}
                 C_o = C_o_(i);
@@ -138,7 +136,8 @@ for j = 1 : nj
             case {'L_E' , 12}
                 L_E = L_E_(i);
             case {'b_f_0' , 13}
-                y0(1) = b_f_0(i);
+                bf0 = b_f_0(i);
+                flag_bf0 = 1;
             case {'H' , 14}
                 H = H_(i);
         end
@@ -146,6 +145,16 @@ for j = 1 : nj
         %-------------- Model assumptions
         Q_f = Q_f/2;    % consider half of the discharge only for one side of the tidal platform (the same will be automatically considered below for Q_T)
         b_fm = b_fm/2;  % consider half of the basin only for one side of the tidal platform
+        
+        %-------------- Initial conditions, y0=[ b_f, d_f, d_m,u(=C_r*(b_f*d_f+b_m*d_m))]
+        if flag_bf0 == 0 % condition for model run for every parameter except for b_f_0
+            y0(1) = b_fm/2;   % tidal flat width (m)
+        else
+            y0(1) = bf0;
+        end
+        y0(2) = 1.0;         % tidal flat depth (m)
+        y0(3) = 0.4;         % marsh depth (m)
+        y0(4) = 0*10^-3*(y0(1)*(y0(2)+y0(3)));
         
         %-------------- Solve the system of differential equations
         [t, y] = ode15s(@ode4marshtidalflat,tspan,y0); % or use ode15s/ode45/..23s
@@ -155,11 +164,12 @@ for j = 1 : nj
     end
     
     %-------------- Plot Results
-%     figure
+    %     figure
+    clf
     plot_BoxModel_SA(t,ydata,leg,tit)
-        print(tit,'-dtiff','-r400')
-        movefile([tit,'.tif'],'C:\Users\fy23\Fateme\Projects\Marsh Model\Results\12 - TF conversion to M corrected')
-        close all
+    %     print(tit,'-dtiff','-r400')
+    %     movefile([tit,'.tif'],'C:\Users\fy23\Fateme\Projects\Marsh Model\Results\12 - TF conversion to M corrected')
+    % %         close all
     
     timespent_min = toc/60
     
@@ -178,19 +188,31 @@ end
             local = b_fm ;
         end
         
-        %-------------- Imposing a condition for when tidal flat turns into marsh
-        flag = 0; % showing that tidal flat is below MSL
+        %-------------- Imposing a condition for tidal flat conversion to marsh in case of presence of new vegetation when tidal flat is above MSL
+        flag_f2m = 0;     % showing that tidal flat is below MSL
         if y(2) <= H
-            flag = 1; % showing that tidal flat is above MSL
+            flag_f2m = 1; % showing that tidal flat is above MSL
         end
+        
+        %-------------- Imposing a condition for marsh conversion to tidal flat in case of death of vegetation when marsh is below MSL
+        %         flag_m2f = 0;     % showing that tidal flat is below MSL
+        %         if y(3) > H
+        %             flag_m2f = 1; % showing that tidal flat is above MSL
+        %         end
         
         %-------------- Model assumptions
         b_m = b_fm-local; % marsh width
-        if flag == 0
+        if flag_f2m == 0
             chi = 2*local+b_r;    % fetch
-        elseif flag == 1
+        elseif flag_f2m == 1
             chi = b_r;
         end
+        
+        %         if flag_m2f == 1 && flag_f2m == 0
+        %             chi = chi + 2*b_m;
+        %         elseif flag_m2f == 1 && flag_f2m == 1
+        %             error('Error: Marsh turned to tidal flat and tidal flat turned to marsh.') %% should be corrected for when the tidal flat is still a tidal flat
+        %         end
         
         %---------------------------------- Define the equations -----------------------------------
         
@@ -200,7 +222,7 @@ end
         %-------------- Compute margin erosion (m/s)
         h = (y(2)+max(0,y(2)-2*H))/2;     % reference water depth
         
-        if  flag==1 || chi<=b_r || v_w==0 % condition for no bed and margin erosion in case of a filled mudflat or no wind
+        if  flag_f2m==1 || chi<=b_r || v_w==0 % condition for no bed and margin erosion in case of a filled mudflat or no wind
             tau = 0; % bed shear stress
             W = 0;   % wave power density
         else
@@ -216,7 +238,7 @@ end
         %-------------- Compute margin accretion (m/s)
         C_r = y(4)/(local*y(2)+b_m*y(3)); % concentration (kg/m3)
         
-        if flag == 0
+        if flag_f2m == 0
             B_a = k_a*omega_s*C_r/rho_s;  % margin accretion
         else
             B_a = 0;
@@ -234,7 +256,7 @@ end
         
         
         %----------------------------- Tidal flat depth changes equation ---------------------------
-        if flag == 0
+        if flag_f2m == 0
             
             %-------------- Compute submerged time when the tidal flat is covered with water
             if y(2) < 2*H
@@ -259,10 +281,10 @@ end
             %         alpha = 0.38 * C_r;
             %         TF_accretion = alpha*dt*(y(2)/H); % based on D'Alpaos et al 2011 approach during one time step
             
-            %-------------- Compute the rate of organice matter production in tidal flat (m/s)
+            %-------------- Compute the rate of organic matter production in tidal flat (m/s)
             SOM = 0;
             
-        elseif flag == 1
+        elseif flag_f2m == 1
             
             %-------------- Compute the rate of bed erosion (m/s)
             TF_erosion = 0;
@@ -270,8 +292,8 @@ end
             %-------------- Compute the rate of sediment accretion (m/s)
             TF_accretion = C_r*y(2)/T_T/rho_s;
             
-            %-------------- Compute the rate of organice matter production in the new marsh (m/s)
-            z_new = H-y(2);            % elevation of marsh platform
+            %-------------- Compute the rate of organic matter production in the new marsh (m/s)
+            z_new = H-y(2);       % elevation of marsh platform
             r_new = -0.5*z_new/H+1;     % reproduction rate
             m_new = 0.5*z_new/H;         % mortality rate
             B_new = B_max*(1-m_new/r_new);  % steady state solution for biomass (kg/m2)
@@ -289,11 +311,15 @@ end
         M_accretion = C_r*y(3)/T_T/rho_s;
         
         %-------------- Compute organice matter production (m/s)
-        z = H-y(3);            % elevation of marsh platform
-        r = -0.5*z/H+1;     % reproduction rate
-        m = 0.5*z/H;         % mortality rate
-        B = B_max*(1-m/r);  % steady state solution for biomass (kg/m2)
-        O = k_B*B;             % organic matter production rate
+        z = H-y(3);       % elevation of marsh platform
+        if z >= 0           % condition for no presence of vegetation when marsh is below MSL
+            r = -0.5*z/H+1;     % reproduction rate
+            m = 0.5*z/H;         % mortality rate
+            B = B_max*(1-m/r);  % steady state solution for biomass (kg/m2)
+            O = k_B*B;            % organic matter production rate
+        else
+            O = 0;
+        end
         
         %-------------- Describe the equation for d_m (m)
         dy(3,1) = - M_accretion - O + R;
@@ -301,14 +327,14 @@ end
         %-------------------------------- Mass conservation equation ------------------------------
         
         %-------------- Compute tidal flat bed erosion (kg/s)
-        if flag == 0
+        if flag_f2m == 0
             bed_erosion = max(0,t_s*E_0*(tau-tau_c)/tau_c*local*L_E);
         else
             bed_erosion = 0;
         end
         
         %-------------- Compute marsh/tidal flat margin erosion/accretion (kg/s)
-        if flag == 0
+        if flag_f2m == 0
             margin = (B_e - B_a)*(y(2)-y(3))*L_E*rho_s;
         else
             margin = 0;
@@ -320,7 +346,7 @@ end
         river_in = Q_f*C_f;
         
         %-------------- Compute deposition on tidal flat (kg/s)
-        if flag == 0
+        if flag_f2m == 0
             %         TF_deposition = t_e*C_r*local*omega_s*L_E;
             TF_deposition = min(t_s*C_r*local*omega_s*L_E, C_r*local*y(2)*L_E/T_T);
             %         TF_deposition = alpha*dt*(y(2)/H)*local*L_E*rho_s;
@@ -332,7 +358,7 @@ end
         M_deposition = C_r*b_m*y(3)*L_E/T_T;
         
         %-------------- Compute export sediment to the ocean (kg/s)
-        if flag == 0
+        if flag_f2m == 0
             export = C_r*local*min(y(2),2*H)*L_E/T_T;
         else
             export = 0;
